7c4c5189784de9dacad9a56fa31baca94613d395
Svyatoslav <sergeyevsvyatoslav@yandex.ru>
2022-02-28 00:07:12 +0300

    ping_task added

--- src/main/java/tech/sergeyev/hedgehogtechtesttask/HedgehogTechTestTaskApplication.java
+++ src/main/java/tech/sergeyev/hedgehogtechtesttask/HedgehogTechTestTaskApplication.java
@@ -2,8 +2,10 @@ package tech.sergeyev.hedgehogtechtesttask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class HedgehogTechTestTaskApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/tech/sergeyev/hedgehogtechtesttask/ping/PingTask.java
@@ -0,0 +1,28 @@
+package tech.sergeyev.hedgehogtechtesttask.ping;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class PingTask {
+    private static final Logger LOGGER = LoggerFactory.getLogger(PingTask.class);
+
+    @Scheduled(fixedRate = 60000)
+    public void ping() {
+        try {
+            URL url = new URL("https://www.google.com");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            LOGGER.info("Ping={}, i'm not sleeping...", url.getHost());
+            connection.disconnect();
+        } catch (IOException e) {
+            LOGGER.info("Ping task failed");
+            e.printStackTrace();
+        }
+
+    }
+}
